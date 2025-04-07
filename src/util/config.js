79a4a96f901425e6417594e4f984625c78f33f32
Sanchit Dang <work.sanchitdang@gmail.com>
2025-04-08 01:29:06 +0530

    corrected base url

--- src/util/config.js
+++ src/util/config.js
@@ -1,3 +1,3 @@
-export const baseURL = "http://flirtzybackend.centralindia.cloudapp.azure.com:5000/admin/adminData";
+export const baseURL = "http://flirtzybackend.centralindia.cloudapp.azure.com:5000/admin/";
 export const secretKey = "shared_secret_key";
 export const projectName = "Video App";
